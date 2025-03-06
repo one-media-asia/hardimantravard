32e380e325e18eec3f7e3633dd8cef96b9ae5566
one-media-asia <onemediaasia@duck.com>
2025-03-06 23:01:09 +0800

    a

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -90,7 +90,7 @@ const Footer = () => {
         {/* Bottom section */}
         <div className="flex flex-col md:flex-row justify-between items-center text-sm text-muted-foreground">
           <div>
-            &copy; {currentYear} Hardiman.se. {t('footer.rights')} <br/><br/><a href="https://onemedia.asia">Powered by One Media Asia Co, Ltd</a>
+            &copy; {currentYear} Hardiman.se. {t('footer.rights')} <br/><br/><a href="https://onemedia.asia">Powered by <www className="OneMedia asia"></www></a>
           </div>
           
           {/* Certifications */}
