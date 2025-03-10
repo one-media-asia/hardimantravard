3a4129b0c52f4121896ecc7b0a5a8a4a7aa596fa
one-media-asia <onemediaasia@duck.com>
2025-03-11 00:03:09 +0800

    switvch

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -84,7 +84,7 @@ const whatsappNumber = "+46733705058";
             {/* Certifications */}
             <div className="flex gap-6 mt-4">
               {/* <span className="text-muted-foreground/80">{t('footer.certified')}</span> */}
-            ..  <a href="https://onemedia.asia" className="hover:text-foreground transition-colors">Powered by www.OneMedia.asia</a>
+             <a href="https://onemedia.asia" className="hover:text-foreground transition-colors">Powered by www.OneMedia.asia</a>
            
               <span className="text-muted-foreground/80">{t('footer.member')}</span>
             </div> 
